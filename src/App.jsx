aa54dc66bfe863b02af33a171ef856429c5267c8
unknown <lucianobeltramino@gmail.com>
2024-08-19 15:55:14 -0300

    Primera Practica UTN

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import React from 'react'
+import PracticaUno from './components/practicaUno/PracticaUno'
+
+const App = () => {
+  return (
+    <PracticaUno/>
+  )
+}
+
+
+export default App
